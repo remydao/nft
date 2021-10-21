adebafb035478407d95ed6c6b9fe07be912cccf3
David Ghiassi <david.ghiassi@gmail.com>
2021-10-21 16:15:20 +0200

    Fix: forgot to remove merge conflict

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,9 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 
 app.use(require('./api/add-user'));
-<<<<<<< HEAD
 app.use(require('./api/add-nft'));
 app.use(require('./api/stats'));
-=======
-app.use(require('./api/best-seller-teams'))
 app.use(require('./api/add-team'));
->>>>>>> ef7ad4395c45abe75cc0fefd4bbf912cc1d16b19
 
 const port: number = 3000;
 app.listen(port, function () {
