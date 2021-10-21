17655c8a9f3d42117d811611a66b83682d1394ad
louis.arrignon <louis.arrignon@gmail.com>
2021-10-21 21:31:10 +0200

    getMostRatedNFT; getLastSells;

--- src/controllers/stats-controller.ts
+++ src/controllers/stats-controller.ts
@@ -1,4 +1,4 @@
-import {User, Team, NFT} from "../sequelize/sequelize";
+import {User, Team, NFT, History} from "../sequelize/sequelize";
 import {Sequelize} from "sequelize";
 
 const getBestSellerTeams = async (req: any, res: any) => {
@@ -11,13 +11,16 @@ const getBestSellerCollections = async (req: any, res: any) => {
 
 const getMostRatedNFTs = async (req: any, res: any) => {
     await NFT.findAll({
-        attributes: [[Sequelize.fn('max', Sequelize.col('rate')), 'max']],
+        attributes: [[Sequelize.fn('max', Sequelize.col('rate')), 'MostRatedNFTs']],
         raw: true
     })
 }
 
 const getLastSells = async (req: any, res: any) => {
-
+    await History.findAll({
+        attributes: [[Sequelize.fn('max', Sequelize.col('rate')), 'LastSells']],
+        raw: true
+    })
 }
 
 const getOwnSells = async (req: any, res: any) => {
