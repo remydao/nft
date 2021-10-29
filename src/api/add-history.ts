ea77f6a77c3ed8782e0c67602c29344d81630bce
louis.arrignon <louis.arrignon@gmail.com>
2021-10-29 17:53:44 +0200

    add swagger for add history

--- src/api/add-history.ts
+++ src/api/add-history.ts
@@ -4,6 +4,47 @@ import {checkAdminTokenMiddleware} from "../services/authorization";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /team:
+ *   post:
+ *     summary: Add an History
+ *     description: Add a new selling history (method to bypass ./sellNFT)
+ *     parameters:
+ *       - in: body
+ *         name: History parameters
+ *         description: History parameters
+ *         schema:
+ *           type: object
+ *           required:
+ *             -nftId
+ *              sellerId
+ *              buyerId
+ *              collectionId
+ *           properties:
+ *              nftId:
+ *                  type: integer
+ *              sellerId:
+ *                  type: integer
+ *              buyerId:
+ *                  type: integer
+ *              collectionId:
+ *                  type: integer
+ *           example:
+ *              nftId: 1
+ *              sellerId: 2
+ *              buyerId: 5
+ *              collectionId: 2
+ *     responses:
+ *       200:
+ *         description: OK
+ *       400:
+ *         description: Incorrect body
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ */
 router.post('/history', addHistory);
 
 module.exports = router;
