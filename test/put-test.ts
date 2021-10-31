dddda26a5a0d49ddf2d3b991422bb0afb2782401
remydao <remy.dao.pro@gmail.com>
2021-10-31 17:35:10 +0100

    Add before() to populate test db with admin

---
+++ test/put-test.ts
@@ -0,0 +1,26 @@
+// During the test the env variable is set to test
+process.env.NODE_ENV = 'test';
+
+// Require the dev-dependencies
+let ch = require('chai');
+let chaiHttp = require('chai-http');
+import {app} from "../src/index";
+let should = ch.should();
+ch.use(chaiHttp);
+
+
+var ready = false;
+
+app.on('ready', function () {
+  ready = true;
+})
+
+describe('PUT tests', () => {
+  before(async function() {
+    while (!ready)
+      await new Promise(r => setTimeout(r, 100));
+  });
+
+  
+
+});
