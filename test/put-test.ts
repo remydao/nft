89c64091dbddfbc6ab5e232a2167886a9b83fa4c
remydao <remy.dao.pro@gmail.com>
2021-10-31 19:52:29 +0100

    added tests

--- test/put-test.ts
+++ test/put-test.ts
@@ -18,6 +18,126 @@ describe('PUT tests', () => {
       await new Promise(r => setTimeout(r, 100));
   });
 
-  
+describe('PUT /admin/balance', () => {
 
+  it('Try to add balance while not logged in', (done) => {
+    let teamBalance = {
+      balance: 5000,
+      teamId: 1
+    }
+
+    ch.request(app)
+    .put('/admin/balance')
+    .send(teamBalance)
+    .end((err: any, res: any) => {
+          res.should.have.status(401);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message');
+      done();
+    });
+  });
+
+  it('Try to add to balance while not admin', (done) => {
+    let login = {
+      email: "trixie@hotmail.fr",
+      password: "testtest"
+    }
+    
+    ch.request(app)
+    .post('/login')
+    .send(login)
+    .end((err: any, res: any) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('access_token');
+          
+          let token = res.body.access_token;
+          
+          let teamBalance = {
+            balance: 5000,
+            teamId: 1
+          }
+
+          ch.request(app)
+          .put('/admin/balance')
+          .set({ "Authorization": `Bearer ${token}` })
+          .send(teamBalance)
+          .end((err: any, res: any) => {
+                res.should.have.status(403);
+                res.body.should.be.a('object');
+                res.body.should.have.property('message').eql("You don't have the permission to access this endpoint");
+                done();
+          });
+    });
+  });
+
+
+    it('Try to add to balance with wrong team ID', (done) => {
+      let login = {
+        email: "gazi@hotmail.fr",
+        password: "testtest"
+      }
+      
+      ch.request(app)
+      .post('/login')
+      .send(login)
+      .end((err: any, res: any) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('access_token');
+            
+            let token = res.body.access_token;
+            
+            let teamBalance = {
+              balance: 5000,
+              teamId: 7
+            }      
+  
+            ch.request(app)
+            .put('/admin/balance')
+            .set({ "Authorization": `Bearer ${token}` })
+            .send(teamBalance)
+            .end((err: any, res: any) => {
+                  res.should.have.status(404);
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('error');
+                  done();
+            });
+      });
+    });
+
+    it('Change balance successfully', (done) => {
+      let login = {
+        email: "gazi@hotmail.fr",
+        password: "testtest"
+      }
+      
+      ch.request(app)
+      .post('/login')
+      .send(login)
+      .end((err: any, res: any) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('access_token');
+            
+            let token = res.body.access_token;
+            
+            let teamBalance = {
+              teamId: 1,
+              balance: 5000
+            }      
+  
+            ch.request(app)
+            .put('/admin/balance')
+            .set({ "Authorization": `Bearer ${token}` })
+            .send(teamBalance)
+            .end((err: any, res: any) => {
+                  res.should.have.status(201);
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('message');
+                  done();
+            });
+      });
+    });
+  });
 });
