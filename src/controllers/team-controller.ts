999dbb5ae62501d541061f6da28344f8fc1b7950
remydao <remy.dao.pro@gmail.com>
2021-10-31 18:39:37 +0100

    Change team management

--- src/controllers/team-controller.ts
+++ src/controllers/team-controller.ts
@@ -18,25 +18,24 @@ const createTeam = async (req: any, res: any) => {
         if (user.TeamId) {
             return handleSpecificError(res, 401, "You are not authorized to add a team because you already have one !");
         }
+        await Team.create(team)
+        .then((createdTeam: any) => {
+            User.update(
+                { TeamId: createdTeam.id },
+                { where: { id: token.id } }
+            );
+            return res.status(200).send(createdTeam);
+        })
+        .catch((err: any) => {
+            console.log(err);
+            if (err.name === "SequelizeValidationError")
+                handleValidationError(err, res);
+            else
+                return handleSpecificError(res, 500, 'Problem with the database');
+        });
     })
     .catch((err: any) => {
         return handleSpecificError(res, 500, 'Problem with the database');
-    });
-
-    await Team.create(team)
-    .then((createdTeam: any) => {
-        User.update(
-            { TeamId: createdTeam.id },
-            { where: { id: token.id } }
-        );
-        return res.status(200).send(createdTeam);
-    })
-    .catch((err: any) => {
-        console.log(err);
-        if (err.name === "SequelizeValidationError")
-            handleValidationError(err, res);
-        else
-            return handleSpecificError(res, 500, 'Problem with the database');
     });
 }
 
@@ -46,35 +45,33 @@ const addToTeam = async (req: any, res: any) => {
         return handleSpecificError(res, 400, "You must have a userId in the body request: id of the user you want to add to your team");
 
     await User.findByPk(req.body.userId)
-    .then((user: any) => {
+    .then(async (user: any) => {
         if (!user)
             return handleSpecificError(res, 400, "The user you try to add to your team does not exist: ID " + req.body.userId + " does not exist")
         if (user.TeamId != null)
             return handleSpecificError(res, 400, "The user you try to add to your team already belongs to a team which id is: " + user.TeamId);
+        
+        const token = extractToken(req.headers.authorization);
+
+        await User.findByPk(token.id)
+        .then(async (user: any) => {
+            if (!user.TeamId) {
+                return handleSpecificError(res, 400, "You have to be in a team to add a user to your team");
+            }
+            await User.update(
+                { TeamId: user.TeamId },
+                { where: { id: req.body.userId } }
+            ).then((addedUser) => {
+                return res.status(200).send({message: "The user " + req.body.userId + " has been added to your team."})
+            });
+        })
+        .catch((err: any) => {
+            return handleSpecificError(res, 500, 'Unknown error');
+        });
     })
     .catch((err: any) => {
         return handleSpecificError(res, 500, 'Problem with the database');
     });
-
-    const token = extractToken(req.headers.authorization);
-
-    await User.findByPk(token.id)
-    .then(async (user: any) => {
-        if (!user.TeamId) {
-            return handleSpecificError(res, 400, "You have to be in a team to add a user to your team");
-        }
-        await User.update(
-            { TeamId: user.TeamId },
-            { where: { id: req.body.userId } }
-        ).then((addedUser) => {
-            return res.status(200).send(addedUser)
-        });
-    })
-    .catch((err: any) => {
-        return handleSpecificError(res, 500, 'Unknown error');
-    });
-
-
 }
 
 const getTeam = async (req: any, res: any) => {
@@ -92,23 +89,23 @@ const addToBalance = async (req: any, res: any) => {
         return handleSpecificError(res, 400, "You must have a correct teamId and a balance (superior to 0) field in the request body");
     
     await Team.findByPk(req.body.teamId)
-    .then((team: any) => {
+    .then(async (team: any) => {
         if (!team)
-            return handleSpecificError(res, 404, "The specified teamId: " + req.body.teamId + ", does not exist.")
+            return handleSpecificError(res, 404, "The specified teamId: " + req.body.teamId + ", does not exist.");
+
+        await Team.update(
+            { balance :  req.body.balance },
+            { where: { id: req.body.teamId } }
+        ).then((team) => {
+            return res.status(201).send({ message: "Balance of the team " + req.body.teamId + " has been changed to " + req.body.balance})
+        })
+        .catch((err: any) => {
+            return handleSpecificError(res, 500, 'Unknown error');
+        });   
     })
     .catch((err: any) => {
         return handleSpecificError(res, 500, 'Unknown error');
     });
-
-    await Team.update(
-        { balance :  req.body.balance },
-        { where: { id: req.body.teamId } }
-    ).then((team) => {
-        return res.status(201).send({ message: "Balance of the team " + req.body.teamId + " has been changed to " + req.body.balance})
-    })
-    .catch((err: any) => {
-        return handleSpecificError(res, 500, 'Unknown error');
-    });   
 }
 
 export { createTeam, addToTeam, getTeam, addToBalance };
